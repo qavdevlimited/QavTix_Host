c4e945b9411d9db3dc85094cddff427dee1e201c
Nelson Onuegbu <leonenelson38@gmail.com>
2026-01-27 08:36:20 +0100

    update to logo href

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Logo({ width = 80, height = 35, logo = logoSrc }: { width?: number; height?: number, logo?: string }) {
     return (
-        <Link href="/" className="inline-block relative z-10">
+        <Link href="/dashboard" className="inline-block relative z-10">
             <Image src={logo} alt="Qavtix Logo" width={width} height={height} />
         </Link>
     )
